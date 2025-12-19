bacdd567330681a605e6097269616425fd6ee9f0
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-19 15:43:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace methodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
